b7dd775b69f24339cab67e0e471eb2be9bd8af68
Elif Chakar <elfrnx@gmail.com>
2026-05-14 20:49:18 +0200

    Remove root-level extension files - keep only linkedin-saver folder

--- background.js
+++
@@ -1,9 +0,0 @@
-// Service Worker - handles background tasks and messaging
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'getProfileData') {
-        // Message handler for getting profile data from content script
-        sendResponse({ status: 'ready' });
-    }
-});
-
-console.log('Background service worker loaded');
